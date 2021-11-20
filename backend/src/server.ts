8c88b74fe88f00117220215057b7d935edf7476b
grpineiro@gmail.com <grpineiro@gmail.com>
2021-11-19 21:17:00 -0300

    Updated: added json configuration.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,18 @@
 import express from "express";
 
+import connection from "./database/connection";
+import UserRoutes from "./app/routes/user.routes";
+import "./database/connection";
 const server = express();
 
 server.get("/", (req, res) => {
   res.json({ test: "Hello World" });
 });
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+server.use(UserRoutes);
 
 server.listen(3000, () => console.log("Listening on port 3000..."));
 
