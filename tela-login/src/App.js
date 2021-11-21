c43029772ab35cf21801a75ecddfa3e1d5f61381
Gabriel Piñeiro <grpineiro@gmai.com>
2021-11-21 19:14:32 -0300

    Added Browser routes.

--- tela-login/src/App.js
+++ tela-login/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom';
 import './App.css'
 
 import Login from './pages/Login'
+import Rotas from './pages/routes';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <Login />
+      <Rotas />
     </div>
+    </BrowserRouter>
+    
   );
 }
 
