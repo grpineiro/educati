6f0a0b3c330f82a44924a8d6ad30c1a383dc710b
GabrielC_dev <gabi_borges01@hotmail.com>
2021-11-21 21:44:37 -0300

    Finished Cadastro without responsivity

--- tela-login/src/pages/Cadastro/index.js
+++ tela-login/src/pages/Cadastro/index.js
@@ -1,68 +1,51 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './cadastro.css'
 
-import { IoEyeOutline, IoEyeOffOutline } from 'react-icons/io5';
-
 import arrow from './images/arrow.png'
 
 function Cadastro() {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [show, setShow] = useState(false)
-
-    const handleClick = (evt) => {
-        evt.preventDefault()
-        setShow(!show);
-    }
-
     return (
         <div className='text-center' id='cadastro'>
             <h1 id='MainTitle'>Cadastro</h1>
 
-            <form>
-                <div class='mb-3' id='inputEmail'>
-                    <input
-                        type='email'
-                        placeholder='Endereço de e-mail'
-                        class='form-control'
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                    />
-                </div>
+            <div className="col-md-7 col-lg-8 d-flex justify-content-center" id='cadastro-usuario'>
+                <form>
+                    <div className="row g-3 justify-content-center" id='itensForm'>
+                        <div className="col-7">
+                            <input type="text" className="form-control" id="nome" placeholder="Nome" required />
+                        </div>
 
-                <div class='mb-3' id='inputPassword'>
-                    <input
-                        type={show ? 'text' : 'password'}
-                        placeholder='Senha'
-                        class='form-control'
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                    />
+                        <div className="col-7">
+                            <input type="text" className="form-control" id="sobrenome" placeholder="Sobrenome" required />
+                        </div>
 
-                    <div className='login-eye'>
-                        {show ? (
-                            <IoEyeOutline
-                                size={20}
-                                onClick={handleClick}
-                            />
-                        ) : (
-                            <IoEyeOffOutline
-                                size={20}
-                                onClick={handleClick}
-                            />
-                        )}
+                        <div className="col-7">
+                            <input type="email" className="form-control" id="email" placeholder="seu@email.com" />
+                        </div>
+
+                        <div className="col-7">
+                            <input type="password" className="form-control" id="password" placeholder="Senha" required />
+                        </div>
+
+                        <div className="col-7">
+                            <input type="password" className="form-control" id="confirmPassword" placeholder="Confirmar senha" />
+                        </div>
+
+                        <div className="col-7">
+                            <input type="date" className="form-control" id="birthday" placeholder='dataNascimento' required />
+                        </div>
                     </div>
-                </div>
+                </form>
 
                 <button type='submit' className='btn btn-success'>Cadastrar</button>
-            </form>
 
-            <div className='forReturn'>
-            <Link to="/"><img src={arrow} className='arrow' alt='Return' /></Link>
-            </div>
-        </div>
+                <div className='forReturn'>
+                    <Link to="/"><img src={arrow} className='arrow' alt='Return' /></Link>
+                </div>
+            </div >
+        </div >
     )
 }
 
