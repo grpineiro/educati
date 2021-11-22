036aad29f00922b6e228aeefb2e5361cc65d67bd
Gabriel Piñeiro <grpineiro@gmai.com>
2021-11-21 23:56:10 -0300

    Added admin routes.

--- backend/src/app/routes/admin.routes.ts
+++ backend/src/app/routes/admin.routes.ts
@@ -1,16 +1,18 @@
 import { Router } from "express";
+import AdminController from "../controllers/AdminController";
 
 const AdminRoutes = () => {
+  const admins = new AdminController();
   const routes = Router();
 
-  routes.get("/list/admin")
-  routes.get("/list/admin/:id");
+  routes.get("/list/admin", admins.getAdmins);
+  routes.get("/list/admin/:id", admins.getAdmin);
 
-  routes.post("/create/admin");
+  routes.post("/create/admin", admins.createAdmin);
 
-  routes.put("/update/admin/:id");
+  routes.put("/update/admin/:id", admins.updateAdmin);
 
-  routes.delete("/delete/admin/:id");
+  routes.delete("/delete/admin/:id", admins.deleteAdmin);
 
   return routes;
 }
