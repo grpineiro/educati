0f33774be04e559b544dfafa442f148058034b72
Gabriel Piñeiro <grpineiro@gmail.com>
2021-11-22 08:40:08 -0300

    Added cors module and configured the same in server.ts file.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,22 @@
 import express from "express";
+import cors from "cors";
 
 import UserRoutes from "./app/routes/user.routes";
 import AdminRoutes from "./app/routes/admin.routes";
 
 import "./database/connection";
+import { appendFile } from "fs";
 
 const server = express();
+
+const corsOptions = {
+  origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+};
+
+server.use(cors(corsOptions));
+
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
@@ -13,4 +24,4 @@ server.use(express.urlencoded({ extended: true }));
 server.use(AdminRoutes);
 server.use(UserRoutes);
 
-server.listen(3000, () => console.log("Listening on port 3000..."));
+server.listen(3333, () => console.log("Listening on port 3333..."));
