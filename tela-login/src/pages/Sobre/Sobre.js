ad0dac5590164059e8c29fab03235f77da83dc7e
JefersonT4v4res <jefersonsaj_tavares@hotmail.com>
2021-11-22 17:04:02 -0300

    Update on screen responsiveness: Sobre
    Co-authored-by: Gabriel Calheiros <GabrielCdev@users.noreply.github.com>

--- tela-login/src/pages/Sobre/Sobre.js
+++ tela-login/src/pages/Sobre/Sobre.js
@@ -8,26 +8,31 @@ import arrowSobre from '../../images/arrow.png'
 
 function Sobre() {
   return (
-    <div className="sobre">
-      <img src={sunSobre} className="Img_Sun_Sobre" alt="imagem sol" />
-      <img src={cloudSobre} className="Img_Cloud_Sobre" alt="imagem nuvem" />
-      <img src={cloudSobre_2} className="Img_Cloud_Sobre_2" alt="imagem nuvem" />
-      <div className="Content_area_Sobre">
-        <h1 className="sobreTitle">EducaTI</h1>
-        <div className="Area_About_Us_Sobre">
-          <p className="About_Us_Sobre">
-            Mussum Ipsum, cacilds vidis litro abertis. Manduma pindureta quium dia nois paga. Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum. Copo furadis é disculpa de bebadis, arcu quam euismod magna. Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis!
-          </p>
-        </div>
+    <div className="text-center" id="sobre">
+      <div for="SunSobre">
+        <img src={sunSobre} id="Img_Sun_Sobre" alt="imagem sol" />
       </div>
-      <footer className="Footer_Sobre">
-        <div className="Arrow_Back_Sobre">
-          <Link to="/"><button ><img src={arrowSobre} className="Img_Arrow_Sobre" alt="imagem Seta" /></button></Link>
-        </div>
-        <div className="Title_page_Sobre">
-          <p className="Title_About_Sobre">Sobre</p>
-        </div>
-      </footer>
+      <div for="CloudUpSobre">
+        <img src={cloudSobre} id="Img_Cloud_Sobre" alt="imagem nuvem" />
+      </div>
+      <div for="CloudDownSobre">
+        <img src={cloudSobre_2} id="Img_Cloud_Sobre_2" alt="imagem nuvem" />
+      </div>
+      
+      <h1 className="sobreTitle">Sobre nós</h1>
+
+      <div className="Area_About_Us_Sobre">
+        <p className="About_Us_Sobre">
+          Somos uma equipe de desenvolvimento formada por 5 amigos que se conheceram na universidade, no curso de Ciência da Computação. Nos unimos com um objetivo em comum: fomentar a educação e o aprendizado de crianças e jovens, trazendo uma forma mais dinâmica e interessante de aprender através de jogos digitais.
+        </p>
+        <p className="About_Us_Sobre2">
+          Com base nisso, temos como principal missão usar desta ferramenta para instigar e desenvolver habilidades como a atenção, imaginação, concentração e raciocínio lógico, além de estimular as habilidades motoras, possibilitando, também, o retorno do prazer ao adquirir conhecimento.
+        </p>
+      </div>
+
+      <div for="returnSobre">
+        <Link to="/"><img src={arrowSobre} className="Img_Arrow_Sobre" alt="imagem Seta" /></Link>
+      </div>
     </div>
   );
 }
