a6144e47994296faf5197553ea17355e3f25a966
GabrielC_dev <gabi_borges01@hotmail.com>
2021-11-22 00:50:17 -0300

    Adjust Cadastro

--- tela-login/src/pages/Cadastro/index.js
+++ tela-login/src/pages/Cadastro/index.js
@@ -8,41 +8,41 @@ import arrow from './images/arrow.png'
 function Cadastro() {
     return (
         <div className='text-center' id='cadastro'>
-            <h1 id='MainTitle'>Cadastro</h1>
+            <h1 id='cadastroTitle'>Cadastro</h1>
 
             <div className="col-md-7 col-lg-8 d-flex justify-content-center" id='cadastro-usuario'>
                 <form>
-                    <div className="row g-3 justify-content-center" id='itensForm'>
+                    <div className="row g-3 justify-content-center" id='itensFormCadastro'>
                         <div className="col-7">
-                            <input type="text" className="form-control" id="nome" placeholder="Nome" required />
+                            <input type="text" className="form-control-cadastro-cadastro" id="nomeCadastro" placeholder="Nome" required />
                         </div>
 
                         <div className="col-7">
-                            <input type="text" className="form-control" id="sobrenome" placeholder="Sobrenome" required />
+                            <input type="text" className="form-control-cadastro-cadastro" id="sobrenomeCadastro" placeholder="Sobrenome" required />
                         </div>
 
                         <div className="col-7">
-                            <input type="email" className="form-control" id="email" placeholder="seu@email.com" />
+                            <input type="email" className="form-control-cadastro" id="emailCadastro" placeholder="seu@email.com" />
                         </div>
 
                         <div className="col-7">
-                            <input type="password" className="form-control" id="password" placeholder="Senha" required />
+                            <input type="password" className="form-control-cadastro" id="passwordCadastro" placeholder="Senha" required />
                         </div>
 
                         <div className="col-7">
-                            <input type="password" className="form-control" id="confirmPassword" placeholder="Confirmar senha" />
+                            <input type="password" className="form-control-cadastro" id="confirmPasswordCadastro" placeholder="Confirmar senha" />
                         </div>
 
                         <div className="col-7">
-                            <input type="date" className="form-control" id="birthday" placeholder='dataNascimento' required />
+                            <input type="date" className="form-control-cadastro" id="birthdayCadastro" placeholder='Data de Nascimento' required />
                         </div>
                     </div>
                 </form>
 
-                <button type='submit' className='btn btn-success'>Cadastrar</button>
+                <button type='submit' className='btnCadastro'>Cadastrar</button>
 
                 <div className='forReturn'>
-                    <Link to="/"><img src={arrow} className='arrow' alt='Return' /></Link>
+                    <Link to="/"><img src={arrow} className='arrowCadastro' alt='Return' /></Link>
                 </div>
             </div >
         </div >
