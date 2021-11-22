c20c1f9fd391a43c6c7d37b4b80d357a9471e572
JefersonT4v4res <jefersonsaj_tavares@hotmail.com>
2021-11-22 19:10:28 -0300

    Pagina Perfil Jeff

--- tela-login/src/pages/Perfil/Perfil.js
+++ tela-login/src/pages/Perfil/Perfil.js
@@ -30,6 +30,7 @@ function Perfil() {
             <p id="Total_Stars_Perfil">{'\u2B50'}100</p>
           </div>
         </div>
+        
         <div className="Options_Nav_Perfil">
           <ul id="Opt_Nav_Perfil">
             <li id="Opt_li_Home_Perfil"><a className="Opt_Home_Perfil" href="#">Home</a></li>
@@ -46,37 +47,37 @@ function Perfil() {
       </nav>
       { /* Fim da Área Navbar */}
 
-      <div className="Container_Perfil"></div>  { /* Faixa azul */}
-
-      { /* Início área usuário */}
-      <div className="Area_Perfil">
-        <div className="Info_Perfil">
-          <div className="Div_Img_Perfil">
-            <img src={token_1} className="Img_Perfil" alt="Imagem perfil" />
-            <a id="Icon_camera_Perfil" href="#"><img src={edit} className="Icon_edit_Perfil" alt="alterar foto perfil" /></a>
-          </div>
-          <p id="Info_Perfil_Name">Jorginho</p>
-        </div>
-
-        <div className="Resumo_Perfil_Info">
-          <div id="Email_Perfil">
-            <div className="Space_Email_Perfil">
-              <p id="p_Email_Perfil">E-mail: </p>
+      { /* Início área usuário 
+      Área lateral esquerda: Foto e nome.*/}
+      <div className="Area_Perfil_User">
+          <div className="Info_Perfil">
+            <div className="Div_Img_Perfil">
+              <img src={token_1} className="Img_Perfil" alt="Imagem perfil" />
+              <a id="Icon_camera_Perfil" href="#"><img src={edit} className="Icon_edit_Perfil" alt="alterar foto perfil" /></a>
             </div>
-            <div id="Space_Email_User_Perfil">
-              <label id="lbl_email_Perfil">email@dominio.com</label>
+            <p id="Info_Perfil_Name">Jorginho</p>
+          </div>
+        { /* Fim área esquerda */}
+
+        { /* Início área direita: Email, Data Nascimento, Qtd. Estrelas */}
+          <div className="Resumo_Perfil_Info">
+            <div id="Email_Perfil">
+              <div className="Space_Email_Perfil">
+                <p id="p_Email_Perfil">E-mail: </p>
+              </div>
+              <div id="Space_Email_User_Perfil">
+                <label id="lbl_email_Perfil">email@dominio.com</label>
+              </div>
+            </div>
+            <div id="Birthday_Perfil">
+              <p id="p_birth_Perfil">Nascimento: </p>
+              <p id="p_date_birth_Perfil">01/01/2000</p>
+            </div>
+            <div id="Stars_Perfil">
+              <p id="p_Stars_Perfil">Estrelas conquistadas:</p>
+              <p id="p_Total_Stars_Perfil">{'\u2B50'}100</p>
             </div>
           </div>
-          <div id="Birthday_Perfil">
-            <p id="p_birth_Perfil">Nascimento: </p>
-            <p id="p_date_birth_Perfil">01/01/2000</p>
-          </div>
-          <div id="Stars_Perfil">
-            <p id="p_Stars_Perfil">Estrelas conquistadas:</p>
-            <p id="p_Total_Stars_Perfil">{'\u2B50'}100</p>
-          </div>
-        </div>
-
       </div>
       { /* Fim área usuário */}
 
