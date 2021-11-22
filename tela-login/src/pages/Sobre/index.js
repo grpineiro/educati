f8d52adf6aad805cefa6ebeca0405ff0eb47c4aa
GabrielC_dev <gabi_borges01@hotmail.com>
2021-11-22 01:07:19 -0300

    Adjust Sobre

--- tela-login/src/pages/Sobre/index.js
+++ tela-login/src/pages/Sobre/index.js
@@ -1,36 +1,35 @@
-import sun from './_imgs/sun.png'
-import cloud from './_imgs/cloud.png'
-import cloud_2 from './_imgs/cloud.png'
-import Arrow from './_imgs/Arrow.png'
-import './App.css';
 import { Link } from 'react-router-dom';
+import './sobre.css';
 
+import sunSobre from '../../images/sun.png'
+import cloudSobre from '../../images/cloud.png'
+import cloudSobre_2 from '../../images/cloud.png'
+import arrowSobre from '../../images/arrow.png'
 
-function App() {
+function Sobre() {
   return (
-    <div className="App">
-      <img src={sun} className="Img_Sun" alt="imagem sol"/>
-      <img src={cloud} className="Img_Cloud" alt="imagem núvem"/>
-      <img src={cloud_2} className="Img_Cloud_2" alt="imagem núvem"/>
-      <div className="Content_area">
-        <h1 className="MainTitle">EducaTI</h1>
-          <div className="Area_About_Us">
-            <p className="About_Us">
+    <div className="sobre">
+      <img src={sunSobre} className="Img_Sun_Sobre" alt="imagem sol" />
+      <img src={cloudSobre} className="Img_Cloud_Sobre" alt="imagem nuvem" />
+      <img src={cloudSobre_2} className="Img_Cloud_Sobre_2" alt="imagem nuvem" />
+      <div className="Content_area_Sobre">
+        <h1 className="sobreTitle">EducaTI</h1>
+        <div className="Area_About_Us_Sobre">
+          <p className="About_Us_Sobre">
             Mussum Ipsum, cacilds vidis litro abertis. Manduma pindureta quium dia nois paga. Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum. Copo furadis é disculpa de bebadis, arcu quam euismod magna. Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis!
-            </p>
-          </div>
+          </p>
+        </div>
       </div>
-      <footer className="Footer">
-          <div className="Arrow_Back">
-          <Link to="/"><button ><img src={Arrow} className="Img_Arrow" alt="imagem Seta" /></button></Link>
-          </div>
-          <div className="Title_page">
-            <p className="Title_About">Sobre</p>
-          </div>
+      <footer className="Footer_Sobre">
+        <div className="Arrow_Back_Sobre">
+          <Link to="/"><button ><img src={arrowSobre} className="Img_Arrow_Sobre" alt="imagem Seta" /></button></Link>
+        </div>
+        <div className="Title_page_Sobre">
+          <p className="Title_About_Sobre">Sobre</p>
+        </div>
       </footer>
     </div>
   );
-
 }
 
-export default App;
+export default Sobre;
