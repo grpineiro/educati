47e33089186dabc21a1f24ac38a9c757bac0c145
Gabriel Piñeiro <grpineiro@gmail.com>
2021-11-27 18:56:23 -0300

    Added jwt authentication to the user routes.

--- backend/src/app/routes/user.routes.ts
+++ backend/src/app/routes/user.routes.ts
@@ -1,19 +1,21 @@
 import { Router } from "express";
 import UserController from "../controllers/UserController";
+import ValidationJWT from "../middlewares/auth";
 
 const UserRoutes = () => {
   const userController = new UserController();
   const routes = Router();
 
-  routes.get("/list/users", userController.getUsers);
-  routes.get("/list/user/:id", userController.getUserById);
-  routes.get("/list/user/", userController.getUserByEmail);
+  routes.get("/list/users", ValidationJWT, userController.getUsers);
+  routes.get("/list/user/:id", ValidationJWT, userController.getUserById);
+  routes.get("/list/user/", ValidationJWT, userController.getUserByEmail);
 
   routes.post("/create/user", userController.createUser);
+  routes.post("/auth/user", userController.loginUser);
 
-  routes.put("/update/user/:id", userController.updateUser);
+  routes.put("/update/user/:id", ValidationJWT, userController.updateUser);
 
-  routes.delete("/delete/user/:id", userController.deleteUser);
+  routes.delete("/delete/user/:id", ValidationJWT, userController.deleteUser);
 
   return routes;
 }
