c0869c5dd46d781c613765f4ef852c23fbad1a2d
Gabriel Piñeiro <grpineiro@gmail.com>
2021-11-28 21:41:44 -0300

    Updated routes.

--- tela-login/src/pages/routes.js
+++ tela-login/src/pages/routes.js
@@ -1,29 +1,51 @@
-import { Routes, Route, useRoutes } from "react-router-dom"
+import { useContext } from "react"
+import { Routes, Route } from "react-router-dom"
 
 import Login from "./Login"
-import Cadastro from "./Cadastro"
-import Sobre from "./Sobre"
-import Perfil from "./Perfil"
-import Home from "./Home"
+import Cadastro from "./Cadastro/cadastro"
+import Sobre from "./Sobre/sobre"
+import Perfil from "./Perfil/perfil"
+import Home from "./Home/home"
 
 import Velha from "./Games/Velha";
 
+import { UserContext } from "../contexts/user.context";
+
 const Rotas = () => {
-  // const routes = useRoutes([
-  //   {path: "/", component: Login, index: true},
-  //   {path: "/sobre", component: Sobre},
-  //   {path: "/cadastro", component: Cadastro}
-  // ])
+  const { signed } = useContext(UserContext);
+  console.log(signed)
+
+  function SignInRoute() {
+    return (
+      <Routes>
+        <Route path="/sobre" element={<Sobre />} />
+        <Route index path="/home" element={<Home />} />
+        <Route path="/perfil" element={<Perfil />} />
+        <Route path="/games/velha" element={<Velha />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/cadastrar" element={<Cadastro />} />
+      </Routes>
+    )
+
+  }
+
+  function SignOutRoute() {
+    return (
+      <Routes>
+        <Route index path="/" element={<Login />} />
+        <Route path="/cadastrar" element={<Cadastro />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/perfil" element={<Perfil />} />
+        <Route path="/games/velha" element={<Velha />} />
+      </Routes>
+    )
+
+  }
 
   return (
-    <Routes>
-      <Route index path="/" element={<Login />} />
-      <Route path="/cadastrar" element={<Cadastro />} />
-      <Route path="/sobre" element={<Sobre />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/perfil" element={<Perfil />} />
-      <Route path="/games/velha" element={<Velha />}/>
-    </Routes>
+    signed ? <SignInRoute /> : <SignOutRoute />
+
   )
 
   // return routes;
