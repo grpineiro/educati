2d35fc75ca2917cf9098ecd93580b5cb3c3df31b
JefersonT4v4res <jefersonsaj_tavares@hotmail.com>
2021-12-05 12:37:13 -0300

    Atualizando css, consertando popUp e dicas

--- frontend/src/pages/Games/Jogo_Forca/components/Notification.js
+++ frontend/src/pages/Games/Jogo_Forca/components/Notification.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Notification = ({ showNotification }) => {
   return (
-    <div id={`notification-container ${showNotification ? "show" : ""}`}>
+    <div className={`notification-container ${showNotification ? "show" : ""}`}>
       <p id="notification_p">Você já selecionou essa letra antes</p>
     </div>
   );
--- frontend/src/pages/Games/Jogo_Forca/components/WrongLetters.js
+++ frontend/src/pages/Games/Jogo_Forca/components/WrongLetters.js
@@ -3,10 +3,10 @@ import React from "react";
 const WrongLetters = ({ wrongLetters }) => {
   return (
     <div id="wrong-letters-container">
-      <div>
+      
         {wrongLetters.length > 0 && <p id="wrong_letters">Letras erradas:</p>}
         {wrongLetters.map((letter, i) => <span id="span_wrongLetters" key={i}>{letter}</span>).reduce((prev, curr) => (prev === null ? [curr] : [prev, ", ", curr]), null)}
-      </div>
+      
     </div>
   );
 };
